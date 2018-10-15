19d0b5aa2b9b1580ce22c7efd2655b785bda09e5
Matt Norris <matthew.norris96@gmail.com>
2018-10-15 23:59:47 +1300

    Add click handling to the Squares

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { isUndefined } from 'util';
 
 const StyledSquare = styled.div`
   border-color: hsla(0, 0%, 0%, 0.2);
   border-style: solid;
-  border-width: 0 
-    ${({ index }) => (index % 3 === 2 ? 0 : '2px')}
-    ${({ index }) => (index < 6 ? '2px' : 0)} 
-    0;
+  border-width: 0 ${({ index }) => (index % 3 === 2 ? 0 : '2px')}
+    ${({ index }) => (index < 6 ? '2px' : 0)} 0;
   color: ${({ player }) => (player === 'x' ? 'hsla(6, 59%, 50%, 1)' : 'hsla(145, 63%, 32%, 1)')};
+  cursor: ${({ onClick }) => (isUndefined(onClick) ? 'default' : 'pointer')}
   font-size: 16vh;
   font-weight: bold;
   line-height: 20vh;
@@ -17,9 +17,9 @@ const StyledSquare = styled.div`
 `
 StyledSquare.displayName = 'StyledSquare'
 
-export default function Square ({ index, player }) {
+export default function Square ({ handleClick, index, player }) {
   return (
-    <StyledSquare index={index} player={player}>
+    <StyledSquare index={index} player={player} onClick={handleClick}>
       {player}
     </StyledSquare>
   )
